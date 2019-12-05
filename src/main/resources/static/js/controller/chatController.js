bab81c2fdd4acbfa9e9649741d3247ee7d4b9421
Git-cwb <cat_cwb@foxmail.com>
2019-12-05 16:43:01 +0800

    js分层

--- src/main/resources/static/js/controller/chatController.js
+++ src/main/resources/static/js/controller/chatController.js
@@ -1,8 +1,8 @@
 var app = angular.module("chat", []);
 
-app.controller("chatController", function ($scope, $http) {
+app.controller("chatController", function ($scope, $http ) {
 
-    //进入登录界面
+    //进入
     $http.get("/chat/findUser").then(function (response) {
         $scope.userMap = response.data;
         console.log("成功进入登录界面！")
